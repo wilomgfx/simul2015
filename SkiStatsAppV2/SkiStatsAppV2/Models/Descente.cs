3d1557b5d266b19d4b202e4162f7245fd63f6bc5
blocdeglace19952 <1250824@LLBD0603INFO21.laboratoire.collegeem.qc.ca>
2015-04-22 10:59:59 -0400

    Descente classe

--- SkiStatsAppV2/SkiStatsAppV2/Models/Descente.cs
+++ SkiStatsAppV2/SkiStatsAppV2/Models/Descente.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace SkiStatsAppV2.Models
 {
     public class Descente
     {
         public int DescenteId { get; set; }
+
+        [ForeignKey("Sortie")]
+        public int SortieId { get; set; }
+        public virtual Sortie Sortie { get; set; }
     }
 }
