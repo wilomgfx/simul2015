3d2b7fa23e30b439cb6170ed6c4d81876d8ae68f
sDesmecht <0934818@LLBD0603INFO20.laboratoire.collegeem.qc.ca>
2015-04-22 10:01:51 -0400

    AppContext
    modification de la version d'entity framework passe de la version 6.1.3
    a 5.0.0 pour un probleme de package

--- SkiStatsApp/SkiStatsApp/DAL/SkiStatsAppContext.cs
+++ SkiStatsApp/SkiStatsApp/DAL/SkiStatsAppContext.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using SkiStatsApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace SkiStatsApp.DAL
 {
-    public class SkiStatsAppContext
+    public class SkiStatsAppContext:DbContext
     {
+        public SkiStatsAppContext() : base() { }
+
+        public DbSet<CentreDeSki> CentreDeSkis { get; set; }
+        public DbSet<Descente> Descentes { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Saison> Saisons { get; set; }
+        public DbSet<Sortie> Sorties { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    
     }
 }
