57d2fb63162e9d968658e7954b601cc014db5a21
sDesmecht <0934818@LLBD0603INFO20.laboratoire.collegeem.qc.ca>
2015-04-22 09:44:11 -0400

    class Region

---
+++ SkiStatsApp/SkiStatsApp/Models/Region.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SkiStatsApp.Models;
+
+namespace SkiStatsApp.Models
+{
+    public class Region
+    {
+        private int RegionId { get; set; }
+
+        private string Nom { get; set; }
+
+        public virtual ICollection<CentreDeSki> CentreDeSki { get; set; }
+    }
+}
