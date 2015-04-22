a8f24decb80d19381f41a5be13a61ed7097db552
Darder006 <joel_x34345@hotmail.com>
2015-04-22 09:37:00 -0400

    Ajout des Models Sortie et Saison

---
+++ SkiStatsApp/SkiStatsApp/Models/Saison.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkiStatsApp.Models
+{
+    public class Saison
+    {
+        public int SaisonId { get; set; }
+
+        public DateTime Annee { get; set; }
+
+
+    }
+}
---
+++ SkiStatsApp/SkiStatsApp/Models/Sortie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkiStatsApp.Models
+{
+    public class Sortie
+    {
+        public int SortieId { get; set; }
+    }
+}
