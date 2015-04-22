53e02b90dc13cedc9af2563a3fc345154eba8c83
blocdeglace19952 <1250824@LLBD0603INFO21.laboratoire.collegeem.qc.ca>
2015-04-22 11:59:39 -0400

    AJOUT DES TABARNAK DE CLASSE DU CALISS DE DAL

--- SkiStatsAppV2/SkiStatsAppV2/DAL/IGenericRepository.cs
+++ SkiStatsAppV2/SkiStatsAppV2/DAL/IGenericRepository.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace SkiStatsAppV2.DAL
 {
-    public class IGenericRepository
+    public interface IGenericRepository<TEntity>
     {
+        IEnumerable<TEntity> Get(Expression<Func<TEntity, bool>> filter = null,
+                                     Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+                                     string includeProperties = "");
+        TEntity GetByID(object id);
+        void Insert(TEntity entity);
+        void Delete(object id);
+        void Update(TEntity entityToUpdate);
     }
 }
