56f294377351362e76d2fc5d54e2f09a444318ab
Darder006 <joel_x34345@hotmail.com>
2015-04-22 15:15:48 -0400

    Mes Requetes 6 a 10

--- SkiStatsAppV2/SkiStatsAppV2/DAL/SaisonRepository.cs
+++ SkiStatsAppV2/SkiStatsAppV2/DAL/SaisonRepository.cs
@@ -10,6 +10,11 @@ namespace SkiStatsAppV2.DAL
     {
         public SaisonRepository(SkiStatsAppV2ContextDbContext context) : base(context) { }
 
+        public Saison ObtenirSaisonComplete(int? id)
+        {
+            return Get(filter: a => a.SaisonId == id, includeProperties: "Sorties").First();
+        }       
+
         public IEnumerable<Saison> GetSaisons() { return Get(); }
 
         public Saison GetSaisonByID(int? id) { return GetByID(id); }
@@ -78,6 +83,127 @@ namespace SkiStatsAppV2.DAL
             }
 
             return nombreDePiedsVerticaux;
+        }
+
+        public decimal ObtenirAverageRunsPerDay(int? id)
+        {
+            Saison saison = ObtenirSaisonComplete(id);
+
+            IEnumerable<Sortie> lstSorties = saison.Sorties;
+
+            int nbrSorties = lstSorties.Count();
+
+            int nbrDescente = 0;
+
+            foreach (Sortie sort in lstSorties)
+            {
+                foreach (Descente desc in sort.Descentes)
+                {
+                    nbrDescente++;
+                }
+            }
+
+            decimal average = (decimal)nbrDescente / (decimal)nbrSorties;
+
+            return average;
+        }
+
+        public decimal ObtenirAverageFeetPerDay(int? id)
+        {
+            Saison saison = ObtenirSaisonComplete(id);
+
+            IEnumerable<Sortie> lstSorties = saison.Sorties;
+
+            int nbrDays = lstSorties.Count();
+
+            int totalFeet = 0;
+
+            foreach (Sortie sort in lstSorties)
+            {
+                foreach (Descente desc in sort.Descentes)
+                {
+                    totalFeet += desc.PiedVerticauxParcourus;
+                }
+            }
+
+
+
+            decimal average = (decimal)totalFeet / (decimal)nbrDays;
+
+            return average;
+        }
+
+        public decimal ObtenirAverageFeetPerRun(int? id)
+        {
+            Saison saison = ObtenirSaisonComplete(id);
+
+            IEnumerable<Sortie> lstSorties = saison.Sorties;
+
+            int nbrDescente = 0;
+            int totalFeet = 0;
+
+            foreach (Sortie sort in lstSorties)
+            {
+                foreach (Descente desc in sort.Descentes)
+                {
+                    nbrDescente++;
+                    totalFeet += desc.PiedVerticauxParcourus;
+                }
+            }
+
+
+
+            decimal average = (decimal)totalFeet / (decimal)nbrDescente;
+
+            return average;
+        }
+
+        public string ObtenirPremiereJourneeDeSki(int? id)
+        {
+            Saison saison = ObtenirSaisonComplete(id);
+
+            IEnumerable<Sortie> lstSorties = saison.Sorties;
+
+            DateTime minDate = DateTime.Now;
+            Sortie premiere = null;
+
+            foreach (Sortie sort in lstSorties)
+            {
+                if (sort.Date < minDate)
+                {
+                    premiere = sort;
+                    minDate = sort.Date;
+                }
+            }
+
+            if (premiere == null)
+                return "";
+
+            return minDate.DayOfWeek.ToString();
+        }
+
+        public string ObtenirPremiereDateDeSki(int? id)
+        {
+            Saison saison = ObtenirSaisonComplete(id);
+
+            IEnumerable<Sortie> lstSorties = saison.Sorties;
+
+            DateTime minDate = DateTime.Now;
+            Sortie premiere = null;
+
+            foreach (Sortie sort in lstSorties)
+            {
+                if (sort.Date < minDate)
+                {
+                    premiere = sort;
+                    minDate = sort.Date;
+                }
+            }
+
+            if (premiere == null)
+                return null;
+
+            return minDate.Month.ToString("mmm") + " " + minDate.Day.ToString();
         }
     }
 }
