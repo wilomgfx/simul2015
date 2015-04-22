fbdc237c08ad0dfd550e31a20ae210a02a25d74e
William Cantin <william@hugocantin.info>
2015-04-22 10:57:53 -0400

    CentreDeSki navigation

--- SkiStatsAppV2/SkiStatsAppV2/Models/CentreDeSki.cs
+++ SkiStatsAppV2/SkiStatsAppV2/Models/CentreDeSki.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,5 +11,10 @@ namespace SkiStatsAppV2.Models
         public int CentreDeSkiId { get; set; }
 
         public string NomDuCentre { get; set; }
+
+        [ForeignKey("Region")]
+        public int RegionId {get;set;}
+
+        public virtual Region Region { get ;set; }
     }
 }
