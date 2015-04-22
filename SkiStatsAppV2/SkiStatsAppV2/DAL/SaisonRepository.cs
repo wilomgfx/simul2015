7432621fbb2fd83c6a73d1b99574a06bea777ff6
William Cantin <william@hugocantin.info>
2015-04-22 15:13:50 -0400

    SaisonRepo Requetes 1 a5

--- SkiStatsAppV2/SkiStatsAppV2/DAL/SaisonRepository.cs
+++ SkiStatsAppV2/SkiStatsAppV2/DAL/SaisonRepository.cs
@@ -19,5 +19,65 @@ namespace SkiStatsAppV2.DAL
         public void UpdateSaison(Saison Saison) { Update(Saison); }
 
         public void DeleteSaison(Saison Saison) { Delete(Saison); }
+
+        public int ObtenirNombreDeJoursSkier(Saison saison)
+        {
+            int nombreDeJours = 0;
+            foreach (Sortie sorties in saison.Sorties)
+            {
+                nombreDeJours++;
+            }
+            return nombreDeJours;
+        }
+        public int ObtenirNombreDeDifferenteRegionsSkier(Saison saison)
+        {
+            int nombreDeDifferenteRegion = 0;
+            List<Region> lstRegion = new List<Region>();
+
+            foreach (Region region in saison.Sorties.Select(r => r.CentreDeSki.Region))
+            {
+                if (!lstRegion.Contains(region))
+                {
+                    lstRegion.Add(region);
+                    nombreDeDifferenteRegion++;
+                }
+            }
+            return nombreDeDifferenteRegion;
+        }
+        public int ObtenirNombreDeNouvelleRegionsSkier(Saison saison)
+        {
+            int nombreDeDifferenteRegion = 0;
+            List<Region> lstRegionDejaSkier = new List<Region>();
+
+            foreach (Region region in saison.Sorties.Select(r => r.CentreDeSki.Region))
+            {
+
+                lstRegionDejaSkier.Add(region);
+                //if (!lstRegionDejaSkier.Contains(regions))
+                //{
+                //    lstRegionDejaSkier.Add(regions);
+                //    nombreDeDifferenteRegion++;
+                //}
+            }
+            foreach (Region region in lstRegionDejaSkier)
+            {
+                if (saison.Sorties.Where(r => r.CentreDeSki.Region == region).Count() != 0)
+                {
+                    nombreDeDifferenteRegion++;
+                }
+            }
+            return nombreDeDifferenteRegion;
+        }
+        public int ObtenirNombreDePiedsVerticaux(Saison saison)
+        {
+            int nombreDePiedsVerticaux = 0;
+
+            foreach (Descente descente in saison.Sorties.Select(r => r.Descentes))
+            {
+                nombreDePiedsVerticaux += descente.PiedVerticauxParcourus;
+            }
+
+            return nombreDePiedsVerticaux;
+        }
     }
 }
