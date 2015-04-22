96f3b7090594121e6190d90177bb8f83400aa8f9
sDesmecht <0934818@LLBD0603INFO20.laboratoire.collegeem.qc.ca>
2015-04-22 09:45:13 -0400

    re class Region
    ajouter s CentreDeSkis pour une collection

--- SkiStatsApp/SkiStatsApp/Models/Region.cs
+++ SkiStatsApp/SkiStatsApp/Models/Region.cs
@@ -12,6 +12,6 @@ namespace SkiStatsApp.Models
 
         private string Nom { get; set; }
 
-        public virtual ICollection<CentreDeSki> CentreDeSki { get; set; }
+        public virtual ICollection<CentreDeSki> CentreDeSkis { get; set; }
     }
 }
